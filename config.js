65a83268555fa92fc629f87b7e06b42eb3105225
Uice Lu <uicestone@gmail.com>
2020-05-09 17:34:21 +0800

    status name

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ export const config = {
   API_ENDPOINT: "https://mini-mars.codeispoetry.tech/api",
   statusLabel: {
     PENDING: "待付款",
-    BOOKED: "待服务",
+    BOOKED: "已确认",
     IN_QUEUE: "排队中",
     CANCELED: "已取消",
-    IN_SERVICE: "服务中",
+    IN_SERVICE: "已入场",
+    PENDING_REFUND: "已冻结",
     FINISHED: "已完成",
   },
 };
